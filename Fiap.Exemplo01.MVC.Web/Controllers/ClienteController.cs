248664e6c753c9c78f89a72ab96338f4a2adc502
BrunoIF <bruno.i.fernandes@hotmail.com>
2016-11-01 11:05:57 -0200

    First Commit

---
+++ Fiap.Exemplo01.MVC.Web/Controllers/ClienteController.cs
@@ -0,0 +1,37 @@
+﻿using Fiap.Exemplo01.MVC.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo01.MVC.Web.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private static List<Cliente> _lista = new List<Cliente>();
+
+        // Abrir a tela
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        // Processar as informações
+        [HttpPost]
+        public ActionResult Cadastrar(Cliente cliente)
+        {
+            _lista.Add(cliente);
+            TempData["msg"] = "Cliente Cadastrado";
+            //ViewBag.EstadoCivil = new SelectList(EstadoCivil, 1);          
+            return RedirectToAction("Cadastrar");
+        }
+
+        public ActionResult Listar()
+        {
+            //ViewBag.lista = _lista; // 1º Opção
+            return View(_lista); // 2º Opção
+        }
+    }
+}
---
+++ Fiap.Exemplo01.MVC.Web/Models/EstadoCivil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Exemplo01.MVC.Web.Models
+{
+    public enum EstadoCivil
+    {
+        Solteiro = 1, Casado = 2, Separado = 3, Divorciado = 4, Viúvo = 5
+    }
+}
